1553c71118369c705ecf5ab149b5e3e78505524f
Islombek <islombekazimov04@gmail.com>
2025-05-13 23:21:29 +0500

    New windows were added
    Grid and StackPanel windows are added

---
+++ ProDay1/LMS/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace LMS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Stack_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new StackPanelWindow();
+            window.Owner = this;
+            window.Show();
+        }
+
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new GridWindow();
+            window.Owner = this;
+            window.Show();
+        }
+    }
+}
